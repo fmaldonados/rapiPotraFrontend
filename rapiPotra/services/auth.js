868622b49d0666b0f1c6e091b1e6bfe114880e82
fmaldonados <felixmaldonado@unitec.edu>
2017-09-19 07:39:15 -0600

    esta fallando el socket

--- rapiPotra/services/auth.js
+++ rapiPotra/services/auth.js
@@ -8,9 +8,12 @@ export default{
 	login(body){
 		return Vue.http.post(url+"login",body);
 	},
+<<<<<<< HEAD
 	loginLocal(body){
 		return Vue.http.post(url+"loginLocal",body);
 	},
+=======
+>>>>>>> esta fallando el socket
 	logout(){
 		return Vue.http.get(url+"logout");
 	}
